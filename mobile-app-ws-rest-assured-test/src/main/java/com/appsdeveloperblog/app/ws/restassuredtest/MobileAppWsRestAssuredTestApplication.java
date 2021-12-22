36d7c9fcc6faca19478d64312beb46290d8c5b7d
teacup11 <ttea.kknezevic@gmail.com>
2021-12-22 09:03:01 +0100

    maven project initilaized with spring start initilizer, added dependencies for rest assured

---
+++ mobile-app-ws-rest-assured-test/src/main/java/com/appsdeveloperblog/app/ws/restassuredtest/MobileAppWsRestAssuredTestApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.app.ws.restassuredtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppWsRestAssuredTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppWsRestAssuredTestApplication.class, args);
+	}
+
+}
---
+++ mobile-app-ws-rest-assured-test/src/test/java/com/appsdeveloperblog/app/ws/restassuredtest/MobileAppWsRestAssuredTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.app.ws.restassuredtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppWsRestAssuredTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
