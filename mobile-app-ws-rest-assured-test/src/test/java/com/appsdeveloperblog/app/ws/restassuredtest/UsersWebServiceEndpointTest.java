4c5b39cea6109778b1ec95670385d5441be40e52
teacup11 <ttea.kknezevic@gmail.com>
2021-12-23 21:22:49 +0100

    get user detials API call: created test method

--- mobile-app-ws-rest-assured-test/src/test/java/com/appsdeveloperblog/app/ws/restassuredtest/UsersWebServiceEndpointTest.java
+++ mobile-app-ws-rest-assured-test/src/test/java/com/appsdeveloperblog/app/ws/restassuredtest/UsersWebServiceEndpointTest.java
@@ -12,6 +12,7 @@ import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestMethodOrder(MethodOrderer.Alphanumeric.class) // tests run in a fixed order
 class UsersWebServiceEndpointTest {
@@ -19,6 +20,9 @@ class UsersWebServiceEndpointTest {
 
     private final String CONTEXT_PATH = "/mobile-app-ws";
     private final String EMAIL_ADDRESS = "ziva.groza@mail.com";
+    private final String JSON = "application/json";
+    private static String authorizationHeader;
+    private static String userId;
 
     @BeforeEach
     void setUp() throws Exception {
@@ -35,21 +39,39 @@ class UsersWebServiceEndpointTest {
         loginDetails.put("password", "123");
 
         // http request
-        Response response = given().contentType("application/json")
-                .accept("application/json")
+        Response response = given().contentType(JSON)
+                .accept(JSON)
                 .body(loginDetails)
                 .when().post(CONTEXT_PATH + "/users/login")
                 .then().statusCode(200)
                 .extract().response();
 
-        String authorizationHeader = response.header("Authorization"); // if this header is there and not null, user login was successful
-        String userId = response.header("UserID");
+        authorizationHeader = response.header("Authorization"); // if this header is there and not null, user login was successful
+        userId = response.header("UserID");
 
         //assert that values not null
         assertNotNull(authorizationHeader);
         assertNotNull(userId);
 
+    }
 
+    @Test
+    final void B_testGetUserDetails() {
+
+        Response response = given()
+                .header("Authorization", authorizationHeader)
+                .accept(JSON)
+                .when().get(CONTEXT_PATH + "/users/" + userId)
+                .then().statusCode(200)
+                .contentType(JSON)
+                .extract().response();
+
+        // assert that response contains user id and user email address
+        String userPublicId = response.jsonPath().getString("userId");
+        String userEmail = response.jsonPath().getString("email");
+        // assert that these values are not null
+        assertNotNull(userPublicId);
+        assertNotNull(userEmail);
     }
 }
 
